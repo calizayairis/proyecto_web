2b195eb9e7ecdfd4a6a505816ab3ad4243c2b86b
calizayairis <iriscalizayac@gmail.com>
2025-12-03 21:38:41 -0400

    Funcionalidad del menú hamburguesa en JavaScript.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const Hambur = document.getElementById("hamburguesa");
+const Menu = document.getElementById("menu");
+Hambur.addEventListener("click", () => {
+    Menu.classList.toggle("activo");
+});
+ 
